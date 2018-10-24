3601a5a8fc5441fafa16ae6c76e57f0e8c657cb5
jurajvandor <433451@mail.muni.cz>
2018-10-24 12:40:44 +0200

    customer dao interface

---
+++ rental-persistence/src/main/java/cz/muni/fi/pa165/project/dao/CustomerDao.java
@@ -0,0 +1,12 @@
+package cz.muni.fi.pa165.project.dao;
+
+import cz.muni.fi.pa165.project.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDao {
+    public void create(Customer customer);
+    public Customer findById(Long id);
+    public List<Customer> findAll();
+    public void delete(Customer customer);
+}
