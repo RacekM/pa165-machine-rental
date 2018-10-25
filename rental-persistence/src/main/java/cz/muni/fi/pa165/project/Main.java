0b58fc46ea5d7ea4d26b2252251378e0805e4e64
jurajvandor <433451@mail.muni.cz>
2018-10-25 20:46:28 +0200

    just basic test

--- rental-persistence/src/main/java/cz/muni/fi/pa165/project/Main.java
+++ rental-persistence/src/main/java/cz/muni/fi/pa165/project/Main.java
@@ -1,9 +1,36 @@
 package cz.muni.fi.pa165.project;
 
+
+import cz.muni.fi.pa165.project.entity.Customer;
+import cz.muni.fi.pa165.project.enums.CustomerType;
+
+import javax.persistence.*;
+
 public class Main {
 
+    @PersistenceUnit
+    private EntityManagerFactory emf;
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        new Main().test();
+    }
+    public void test(){
+        //should be in constructor
+        emf = Persistence.createEntityManagerFactory("default");
+
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction t = entityManager.getTransaction();
+        t.begin();
+        Customer c = new Customer("jozo", CustomerType.INDIVIDUAL);
+        entityManager.persist(c);
+        t.commit();
+        entityManager.close();
+        entityManager = emf.createEntityManager();
+        t = entityManager.getTransaction();
+        t.begin();
+        System.out.append(entityManager.find(Customer.class, c.getId()).toString());
+        t.commit();
+        entityManager.close();
     }
 
 }
