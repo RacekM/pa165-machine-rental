ac7d4ab42da502019a2efc25c40905d663ffab8d
jurajvandor <433451@mail.muni.cz>
2018-10-27 17:40:38 +0200

    added javadoc

--- rental-persistence/src/main/java/cz/muni/fi/pa165/project/dao/MachineDaoImpl.java
+++ rental-persistence/src/main/java/cz/muni/fi/pa165/project/dao/MachineDaoImpl.java
@@ -18,26 +18,45 @@ public class MachineDaoImpl implements MachineDao {
     @PersistenceContext
     private EntityManager entityManager;
 
+    /**
+     * persists entity machine
+     * @param machine object to persist
+     */
     @Override
     public void create(Machine machine) {
         entityManager.persist(machine);
     }
 
+    /**
+     * updates entity in DB (based on ID)
+     * @param machine object to update
+     */
     @Override
     public void update(Machine machine) {
         entityManager.merge(machine);
     }
 
+    /**
+     * @param id id of machine to be found
+     * @return object of class machine with given id
+     */
     @Override
     public Machine findById(Long id) {
         return entityManager.find(Machine.class, id);
     }
 
+    /**
+     * @return list of all machines in DB
+     */
     @Override
     public List<Machine> findAll() {
         return entityManager.createQuery("select m from Machine m", Machine.class).getResultList();
     }
 
+    /**
+     * deletes given machine from database
+     * @param machine object to be deleted
+     */
     @Override
     public void delete(Machine machine) {
         entityManager.remove(entityManager.merge(machine));
