193855b90be8e63ef3978048af9b10359605526b
Adam Vaňko <xvanko@fi.muni.cz>
2018-10-27 13:15:14 +0200

    Change id generating strategy to IDENTITY.

--- rental-persistence/src/main/java/cz/muni/fi/pa165/project/entity/Machine.java
+++ rental-persistence/src/main/java/cz/muni/fi/pa165/project/entity/Machine.java
@@ -11,7 +11,7 @@ import java.util.Objects;
 public class Machine {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
