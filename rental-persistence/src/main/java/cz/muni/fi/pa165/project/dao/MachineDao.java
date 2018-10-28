7c7d47cfb43d8df579cf77d9431707ed33a23e56
Matúš Raček <445411@mail.muni.cz>
2018-10-28 09:25:19 +0100

    added javadocs

--- rental-persistence/src/main/java/cz/muni/fi/pa165/project/dao/MachineDao.java
+++ rental-persistence/src/main/java/cz/muni/fi/pa165/project/dao/MachineDao.java
@@ -5,18 +5,44 @@ import cz.muni.fi.pa165.project.entity.Machine;
 import java.util.List;
 
 /**
- * Dao interface for machine entity
+ * DAO interface for entity {@link Machine}.
  *
  * @author Matus Racek (mat.racek@gmail.com)
  */
 public interface MachineDao {
+    /**
+     * Persist machine in data storage.
+     *
+     * @param machine machine to persist
+     */
     void create(Machine machine);
 
+    /**
+     * Update machine in data storage.
+     *
+     * @param machine machine to update
+     */
     void update(Machine machine);
 
+    /**
+     * Finds machine by id.
+     *
+     * @param id id number of the rental
+     * @return Found machine if data storage contains it, null otherwise.
+     */
     Machine findById(Long id);
 
+    /**
+     * Finds all machines
+     *
+     * @return List of all machines
+     */
     List<Machine> findAll();
 
+    /**
+     * Deletes machine from data storage.
+     *
+     * @param machine machine to remove
+     */
     void delete(Machine machine);
 }
