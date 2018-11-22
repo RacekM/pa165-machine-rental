62c85ea8d69655be7df8bd7384d909c19406c2a9
Adam Vaňko <xvanko@fi.muni.cz>
2018-11-22 17:51:44 +0100

    Service layer for rental.

---
+++ rental-service/src/main/java/cz.muni.fi.pa165.project.service/RentalServiceImpl.java
@@ -0,0 +1,52 @@
+package cz.muni.fi.pa165.project.service;
+
+import cz.muni.fi.pa165.project.dao.RentalDao;
+import cz.muni.fi.pa165.project.entity.Customer;
+import cz.muni.fi.pa165.project.entity.Rental;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * Implementation of the {@link RentalService}.
+ *
+ * @author Adam Vanko (445310@mail.muni.cz)
+ */
+@Service
+public class RentalServiceImpl implements RentalService {
+
+    @Inject
+    private RentalDao rentalDao;
+
+    @Override
+    public void create(Rental rental) {
+        rentalDao.create(rental);
+    }
+
+    @Override
+    public void update(Rental rental) {
+        rentalDao.update(rental);
+    }
+
+    @Override
+    public Rental findById(Long id) {
+        return rentalDao.findById(id);
+    }
+
+    @Override
+    public List<Rental> findByCustomer(Customer customer) {
+        return rentalDao.findByCustomer(customer);
+    }
+
+    @Override
+    public List<Rental> findAll() {
+        return rentalDao.findAll();
+    }
+
+    @Override
+    public void remove(Rental rental) {
+        rentalDao.delete(rental);
+    }
+
+}
